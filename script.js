c7e75d898c1112f27f919138ecf8699e25b999a1
Obscure-Git <dhakalkritish7@gmail.com>
2023-02-10 21:41:06 +0545

    -

---
+++ script.js
@@ -0,0 +1,37 @@
+const apiKey = "68aee6f8197f0bc8fe5ea59b909d746d";
+let weather = {
+  fetchWeather: function (city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerHTML = `Weather in ${name}`;
+    document.querySelector(".icon").src = `http://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").innerHTML = description;
+    document.querySelector(".temp").innerHTML = `${temp}°C`;
+    document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").innerHTML = `Wind Speed: ${speed}km/h`;
+    document.querySelector(".card").classList.remove("loading");
+    document.querySelector(".card").classList.add("loaded");
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+}); 
+weather.fetchWeather("Kathmandu");
